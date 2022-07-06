6497196d639b69a6333cc5c16639067227b89d96
whx <1300610385@qq.com>
2022-07-06 15:12:46 +0800

    push commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -9,5 +9,6 @@ public class hello {
         System.out.println("hot-fix");
         System.out.println("master");
         System.out.println("hot-fix2");
+        System.out.println("hhhhhhhhh");
     }
 }
